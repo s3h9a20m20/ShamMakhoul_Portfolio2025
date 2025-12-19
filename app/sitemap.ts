1e80baedf30be9985ebc9ae872c882df5556c4f0
sham-makhoul3 <makhoulsham@gmail.com>
2025-12-19 14:00:10 +0300

    Fix sitemap for static export on Netlify

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,13 @@
 import type { MetadataRoute } from 'next';
 
+// هذه السطور الجديدة لحل المشكلة
+export const dynamic = 'force-static';
+export const revalidate = false;
+
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://me.toinfinite.dev',
+            url: 'https://me.toinfinite.dev', // غيّر الرابط ده بعدين للرابط النهائي بتاعك
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1,
